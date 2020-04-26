9ce2604284c1880a1b590b7ec55cbd93c59faf4d
Alamgir Hossain <alamgir.h@cryptenet.com>
2020-04-26 15:06:59 +0600

    [exception_handling] - topics on exception handling added

--- src/com/cryptenet/cqm/Main.java
+++ src/com/cryptenet/cqm/Main.java
@@ -1,8 +1,40 @@
 package com.cryptenet.cqm;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public static void main(String[] args) throws Exception {
+//        try {
+//            int div = 1/0;
+//        } catch (ArithmeticException exception) {
+//            throw new ArithmeticException("Cannot divide by zero!!");
+//        } catch (ArrayIndexOutOfBoundsException exception) {
+//            throw new ArrayIndexOutOfBoundsException("You are out of your limit!!");
+//        } finally {
+//            throw new IllegalArgumentException("You got botched!!");
+//        }
+
+        try {
+            int div = 1/0;
+        } catch (Exception exception) {
+            throw new Exception("Cannot divide by zero!!");
+        }
+
+//        try {
+//            int div = 1/0;
+//        } catch (ArithmeticException | ArrayIndexOutOfBoundsException exception) {
+//            throw new ArithmeticException("You did something wrong mate!!");
+//        } finally {
+//            throw new IllegalArgumentException("You got botched!!");
+//        }
+
+        try(Scanner scanner = new Scanner(new File("file.txt"))) {
+            //read file
+        } catch (FileNotFoundException exception) {
+            // handle exception
+        }
     }
 }
